61141f09271370efadf0e97f90fc383b14dbc7a2
CaityJ98 <caitlynjschaper@gmail.com>
2022-08-07 21:08:44 +1000

    added heroku path to package.json

--- studentsaver/server/index.js
+++ studentsaver/server/index.js
@@ -16,14 +16,12 @@ app.use(auth());
 connectDB();
 
 app.use(routes);
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging") { 
-  app.use(express.static("client/public"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "./client/public/index.html"))
+
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "./client/build/index.html"))
   })
-}
+
 
 
 http.listen(PORT, () =>
